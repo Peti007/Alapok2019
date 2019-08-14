a13897fa5a6771c61f8774d3f87d8ec51e11f4b6
Péter Joó <jpeti91@hotmail.com>
2019-08-14 21:22:43 +0200

    02SikidomokTerulete

---
+++ 02Nap/02SikidomokTerulete/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02SikidomokTerulete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var negyzet = new Square(side: 4);
+
+            //ez helyett:
+            //"A négyzet területe: " + square.Area().ToString()"
+            Console.WriteLine($"négyzet területe:{negyzet.Area()}");
+
+            var circle = new Circle(radius: 5);
+            Console.WriteLine($"kör területe:{circle.Area()}");
+
+            var triangle = new Triangle(trianglebase: 6, height: 4);
+            Console.WriteLine($"háromszög területe:{triangle.Area()}");
+
+            Console.ReadLine();
+        }
+    }
+}

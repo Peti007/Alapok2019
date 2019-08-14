bfaa13aa61e28edec285a0f120200844720c3ae1
Péter Joó <jpeti91@hotmail.com>
2019-08-14 21:48:22 +0200

    terület összeadása szokásos módon

--- 02Nap/02SikidomokTerulete/Program.cs
+++ 02Nap/02SikidomokTerulete/Program.cs
@@ -6,17 +6,27 @@ namespace _02SikidomokTerulete
     {
         static void Main(string[] args)
         {
-            var negyzet = new Square(side: 4);
+            var square = new Square(side: 4);
 
             //ez helyett:
             //"A négyzet területe: " + square.Area().ToString()"
-            Console.WriteLine($"négyzet területe:{negyzet.Area()}");
+            Console.WriteLine($"négyzet területe:{square.Area()}");
 
             var circle = new Circle(radius: 5);
             Console.WriteLine($"kör területe:{circle.Area()}");
 
             var triangle = new Triangle(trianglebase: 6, height: 4);
             Console.WriteLine($"háromszög területe:{triangle.Area()}");
+
+            var areasum = square.Area();
+
+            areasum = areasum + circle.Area();
+
+            areasum += triangle.Area();
+
+            //profibb megoldás
+
+
 
             Console.ReadLine();
         }

232725fc498db3ad88fd95da2535ca6407e70c8f
Péter Joó <jpeti91@hotmail.com>
2019-08-29 19:16:22 +0200

    04Nap

---
+++ 04Nap/01ConstructorFinalizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01ConstructorFinalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

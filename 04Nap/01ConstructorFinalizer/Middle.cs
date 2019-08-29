61e38ba8ccd4f3ee9ae914df95a5b3d579917a46
Péter Joó <jpeti91@hotmail.com>
2019-08-29 20:34:57 +0200

    #5: leszármaztatott osztályok létrehozóinak (osztályokon átívelő) együttműködése

--- 04Nap/01ConstructorFinalizer/Middle.cs
+++ 04Nap/01ConstructorFinalizer/Middle.cs
@@ -2,9 +2,14 @@
 {
     public class Middle : Base
     {
-        public Middle()
+        //public Middle()
+        //{
+        //    System.Console.WriteLine("Middle létrehozó: Middle()");
+        //}
+
+        public Middle(string name, string email) : base(name, email)
         {
-            System.Console.WriteLine("Middle létrehozó: Middle()");
+            System.Console.WriteLine("Middle létrehozó: Middle(string name, string email)");
         }
     }
 }
--- 04Nap/01ConstructorFinalizer/Program.cs
+++ 04Nap/01ConstructorFinalizer/Program.cs
@@ -8,14 +8,22 @@ namespace _01ConstructorFinalizer
         {
             var b = new Base();
             Console.WriteLine();
-            var m = new Middle();
-            Console.WriteLine();
-            var t = new Third();
-            Console.WriteLine();
+            //var m = new Middle();
+            //Console.WriteLine();
+            //var t = new Third();
+            //Console.WriteLine();
             b = new Base("ez az alaposztály");
             Console.WriteLine();
 
             b = new Base("ez az alaposztály","alap@alap.hu");
+            Console.WriteLine();
+
+            var m = new Middle("ez a middle","middle@middle.hu");
+            Console.WriteLine($"név: {m.Name}, email: {m.Email}");
+            Console.WriteLine();
+
+            var t = new Third("ez a third", "third@third.com");
+            Console.WriteLine($"név: {t.Name}, email: {t.Email}");
             Console.WriteLine();
 
 

1e0b78e6a3f1222e3ee32117dae4a9c92e400bfb
Péter Joó <jpeti91@hotmail.com>
2019-09-01 21:25:06 +0200

    #7 IDisposable implementáció 1. rész vázlat

---
+++ 05Nap/01IDisposable/ItselfCleaner.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace _01IDisposable
+{
+    /// <summary>
+    /// Saját maga után takarítani képes osztály készítése
+    /// a beípitett IDisposable felület segítségével
+    /// 
+    /// Mikor kell megunk után takarítani?
+    /// 
+    /// - külső függőségünk van (file, hálózat, stb) esetén, ha menedzselt objektumot használunk, akkor a GC megold mident
+    ///  - kivéve ha a menedzselt kód IDisposable mintát használ, mert akkor nekünk is kötelező ezt implementálni
+    /// - nagy memóriát használunk, akkor javasolt megunk után takarítani
+    /// -direkt memóriakezelést használunk6 nem menedzselt memóriakezelés
+    /// 
+    /// ha takarítani való függőségei vannak 
+    /// </summary>
+
+
+    public class ItselfCleaner : IDisposable 
+    {
+        private bool isDisposed = false;
+
+        //menedzslet stream, de IDisposable felületet implementál
+        private Stream fileStream = new FileStream("file.text",FileMode.Create);
+        //menedzselt lista, de nagy méretű
+        private List<string> managedMemory = new List<string>();
+        //nem menedzselt memóriaterület
+        private IntPtr unmanagedMemory = IntPtr.Zero;
+
+        public ItselfCleaner()
+        {
+            //menedzselt memória feltöltése
+            for (int i = 0; i < 1000000; i++)
+            {
+                managedMemory.Add("AAAAAAAA");
+            }
+
+            unmanagedMemory = Marshal.AllocHGlobal(1000000);
+            //mivel nem menedzslet memóriaterületről van szó, így szólnunk kell a GC-nek, hogy ezt a területet már nem használhatja
+            GC.AddMemoryPressure(1000000);
+
+        }
+
+
+        /// <summary>
+        /// A takarítást végző függvény
+        /// 
+        /// dispose függvényben nem szabad kivételt okoznunk
+        /// </summary>
+        public void Dispose()
+        {
+            //takarítunk
+            if (isDisposed)
+            {
+                return;
+            }
+
+
+            //menedzselt IDisposablet használó példány felszabadítása
+            fileStream.Dispose();
+            fileStream = null;
+
+            //menedzselt memória felszbadítása
+            managedMemory.Clear();
+            managedMemory = null;
+
+
+            //nem menedzslet memória felszabadítása
+            Marshal.FreeHGlobal(unmanagedMemory);
+            //szólunk a GC-nek, hogy újra használhatja ezt a területet
+
+            GC.RemoveMemoryPressure(1000000);
+
+            //takarítás
+            isDisposed = true;
+        }
+
+        ~ItselfCleaner()
+        {
+            Dispose();
+        }
+
+    }
+}
---
+++ 05Nap/01IDisposable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01IDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////készítünk egy önmaga után rednet hagyó/ maga után takarító osztályt
+            //var takarito = new ItselfCleaner();
+
+
+            //try //megpróbálja a kódblokkot végrehajtani
+            //{
+            //    //elvégezzük a dolgunk
+            //}
+
+            //finally //miután az előzőek lefutottak, akár hiba van akár nem, ez a kódblokk mindenképpen lefut
+            //{
+            //    //az IDisposable felület dispose függvényét használjuk
+            //    ((IDisposable)takarito).Dispose();
+            //}
+
+
+            //hogy ne kelljen mindig ezt a sok kódot leírni, ezért
+            //ilyen 'syntactic sugar' használatával a fordító generálja nekünk ugyanezt
+            using (var takarit = new ItselfCleaner())
+            {//ebben a kódblokkban van a védendő használata (ez a try ág)
+
+
+            }
+
+        }
+    }
+}

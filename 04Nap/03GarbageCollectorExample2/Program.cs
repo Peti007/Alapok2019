8fa586ac99444d5265d2c3a40dedd704159f3e2e
Péter Joó <jpeti91@hotmail.com>
2019-09-01 19:10:00 +0200

    példakód a memóriakezelésre és a finalizer helyes használatára (IDisposable)

---
+++ 04Nap/03GarbageCollectorExample2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace _03GarbageCollectorExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!Console.KeyAvailable)
+            {
+                //var bitmap = new Bitmap(2480,2048);
+
+                using (var bitmap = new Bitmap(2480, 2048)) { }
+            }
+
+
+            
+        }
+    }
+}

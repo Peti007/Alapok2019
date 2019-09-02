eb7dd915e21f801ebcb30eb02287b1ffa9b40d2b
Péter Joó <jpeti91@hotmail.com>
2019-09-02 20:43:03 +0200

    #8 IEnumerable<T> függvény használata és a statikus függvények

---
+++ 05Nap/02IEnumerable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in ShoppingList())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static IEnumerable<string> ShoppingList()
+        {
+            yield return "1 kg marhahús";
+            yield return "só";
+            yield return "1 kg burgonya";
+            yield return "1 kg liszt";
+
+            ShoppingList();
+            Main(new string[] { });
+        }
+        public void InstanceFunction() {
+            ShoppingList();
+            Main(new string[] { });
+
+        }
+    }
+}

cf7764f9ad36e73a13f4af8d56deba3af7eb6228
Péter Joó <jpeti91@hotmail.com>
2019-09-10 20:22:12 +0200

    #1: Naplózás: programozó oldala

--- 07Nap/06Log4Net/Program.cs
+++ 07Nap/06Log4Net/Program.cs
@@ -9,8 +9,15 @@ namespace _06Log4Net
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+        //ugyanaz
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger("_06Log4Net.Program");
+
+
         static void Main(string[] args)
         {
+            log4net.Config.XmlConfigurator.Configure();
+
             var r = new Random();
 
             while (!Console.KeyAvailable)
@@ -19,26 +26,26 @@ namespace _06Log4Net
 
                 if (level<50)
                 {
-
+                    log.Debug($"Debug üzenet: {level}");
                 }
 
                 if (level>=50 && level<70)
                 {
-
+                    log.Info($"Info üzenet: {level}");
                 }
 
                 if (level>=70 && level<85)
                 {
-
+                    log.Warn($"Warn üzenet: {level}");
                 }
                 if (level>=85 && level<95)
                 {
-
+                    log.Error($"Erro üzenet: {level}");
                 }
 
                 if (level>=95)
                 {
-
+                    log.Fatal($"Fatal üzenet: {level}");
                 }
                 Thread.Sleep(200);
             }

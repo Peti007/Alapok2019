c2cf8987a7d6b765518b92301ff9cdf83446fa49
Péter Joó <jpeti91@hotmail.com>
2019-09-14 20:08:24 +0200

    #11: Stratégia minta problémafelvetés

---
+++ 08Nap/08StrategyPattern/DataStore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _08StrategyPattern
+{
+    internal class DataStore
+    {
+        private int[] data;
+
+        public DataStore(int[] data)
+        {
+            this.data = data;
+        }
+
+        public int SumOfOdd()
+        {
+            var sum = 0;
+            foreach (var d in data)
+            {
+                if (d % 2 == 1)
+                {
+                    sum += d;
+                }
+            }
+            return sum;
+
+            //return data.Sum(x => x % 2 == 1);
+        }
+
+        public int ProductOfEven()
+        {
+            var prod = 1;
+            foreach (var d in data)
+            {
+                if (d % 2 == 0)
+                {
+                    prod *= d;
+                }
+            }
+            return prod;
+        }
+    }
+}

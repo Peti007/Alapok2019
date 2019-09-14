e62c422880912a00d01e6130e30b9cabbdb930e1
Péter Joó <jpeti91@hotmail.com>
2019-09-14 19:13:19 +0200

    #1: Serolig naplózás a Console-ra.

---
+++ 08Nap/07Serilog/Program.cs
@@ -0,0 +1,48 @@
+﻿using Serilog;
+using System;
+using System.Threading;
+
+namespace _07Serilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Serilog.Log.Logger = new Serilog.LoggerConfiguration()
+                                                                .WriteTo.Console()
+                                                                .CreateLogger();
+            var r = new Random();
+
+            while (!Console.KeyAvailable)
+            {
+                var level = r.Next(100);
+
+                if (level < 50)
+                {
+                    Log.Logger.Debug($"Debug: {level}");
+                }
+
+                if (level >= 50 && level < 70)
+                {
+                    Log.Logger.Information($"Information: {level}");
+                }
+
+                if (level >= 70 && level < 85)
+                {
+                    Log.Logger.Warning($"Warning: {level}");
+                }
+                if (level >= 85 && level < 95)
+                {
+                    Log.Logger.Error($"Error: {level}");
+                }
+
+                if (level >= 95)
+                {
+                    Log.Logger.Fatal($"Fatal: {level}");
+                }
+                Thread.Sleep(200);
+            }
+        }
+    }
+}

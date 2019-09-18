644708bb58e15b03f96e07e2e7f5c2c2d54b55be
Péter Joó <jpeti91@hotmail.com>
2019-09-18 20:01:52 +0200

    #11: Probléma felvetés hosszú folyamat ami értesíti a többi résztvevőt

---
+++ 10Nap/01ObserverPattern/LongRunningProcess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace _01ObserverPattern
+{
+    public class LongRunningProcess
+    {
+        private Logger log;
+        private UserInterface ui;
+
+        public LongRunningProcess(Logger log, UserInterface ui)
+        {
+            this.log = log;
+            this.ui = ui;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("LongRunningProcess: 0%");
+            log.Message(0);
+            ui.Message(0);
+            Thread.Sleep(1000);
+            Console.WriteLine("LongRunningProcess: 25%");
+            log.Message(25);
+            ui.Message(25);
+            Thread.Sleep(1000);
+            Console.WriteLine("LongRunningProcess: 50%");
+            log.Message(50);
+            ui.Message(50);
+            Thread.Sleep(1000);
+            Console.WriteLine("LongRunningProcess: 75%");
+            log.Message(75);
+            ui.Message(75);
+            Thread.Sleep(1000);
+            Console.WriteLine("LongRunningProcess: 100%");
+            log.Message(100);
+            ui.Message(100);
+        }
+    }
+}
---
+++ 10Nap/01ObserverPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //két alkalmazás modul, ami igényt tart az információra
+            //naplózás
+            var log = new Logger();
+            //felhasználói felület
+            var ui = new UserInterface();
+
+            //hosszantartó folyamat
+            var process = new LongRunningProcess(log, ui);
+
+            process.Start();
+
+            Console.WriteLine("A folyamat lefutott!");
+            Console.ReadLine();
+        }
+    }
+}

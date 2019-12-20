9c4c9eaea6078c042552e3225f3bdd916579b2d9
Péter Joó <jpeti91@hotmail.com>
2019-12-20 14:23:52 +0100

    #15:  Data first a .net framework/ entity framework párossal

---
+++ 11Nap/05DataFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05DataFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new CodeFirstDBEntities();
+
+            Console.WriteLine($"Tanárok: {db.Teachers.Count()}, Tárgyak: {db.Subjects.Count()}");
+
+            Console.ReadLine();
+        }
+    }
+}

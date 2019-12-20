93f777cec6a4f824b7101cedae7d4a480bab7833
Péter Joó <jpeti91@hotmail.com>
2019-12-20 11:00:39 +0100

    #14: db létezésének vizsgálata, adatok feltöltése

--- 11Nap/03CodeFirstCore/Program.cs
+++ 11Nap/03CodeFirstCore/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using _03CodeFirstCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03CodeFirstCore
 {
@@ -6,7 +10,37 @@ namespace _03CodeFirstCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionString = "Server=.\\SQLEXPRESS;Database=CodeFirstDB;Trusted_Connection=True;";
+
+            var optionsBuilder = new DbContextOptionsBuilder<CodeFirstContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            var db = new CodeFirstContext(optionsBuilder.Options);
+
+
+            db.Database.Migrate();
+            if (db.Teacher.Count() == 0)
+            {// adatok ellenőrzése
+                Seed(db);
+            }
+
+           
+
+            Console.WriteLine($"A tanárok száma: {db.Teacher.Count()}, tantárgyak száma: {db.Subject.Count()}");
+
+            Console.ReadLine();
+        }
+
+        private static void Seed(CodeFirstContext db)
+        {
+            var subjects = new List<Subject>();
+
+            subjects.Add(new Subject() { Name = "Matek" });
+            subjects.Add(new Subject() { Name = "Fizika" });
+
+            db.Teacher.Add(new Teacher() { Name = "Matektanár", Subject = subjects });
+            db.SaveChanges();
         }
     }
 }
